5609a632b101603c48ab24b40ff92b2d7cf3a127
arunprince-minidreams <arunprince@minidreams.in>
2025-04-13 10:48:58 +0530

    Conditional branching completed

---
+++ 4. Javascript/8. Conditional Branching/script.js
@@ -0,0 +1,18 @@
+// prompt
+// age, diseases, bloodGroup, name, gender, bloodPressure, alcohol, weight
+
+let name = prompt("Enter your name");
+let age = Number(prompt("Enter your age"));
+let bloodGroup = prompt("Enter your blood group");
+let bloodPressure = Number(prompt("Enter your blood pressure"));
+let alcohol = prompt("is donar consumed alcohol?");
+let diseases = prompt("is Donar has any diseases?");
+let weight = Number(prompt("Enter your weight in Kgs"));
+
+// 1. age                       >= 18
+// 2. weight                    >= 60
+// 3. blood group               true
+// 4. blood pressure            <= 100
+// 5. alcohol                   false
+// 6. diseases                  false
+// 7. name                      true
